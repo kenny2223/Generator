21102e6ac626beea847579eb9a08d17d619c2398
kenny2223 <38969256+kenny2223@users.noreply.github.com>
2023-07-16 10:33:58 -0600

    Add files via upload

---
+++ generator.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#Script Develop for Ing.Kenny Ortiz
+
+wget https://github.com/kenny2223/Generator/blob/main/generator.tar.gz?raw=true -O generator.tar.gz
+
+tar -xvzf generator.tar.gz
+
+cd generator
+
+#make the menu.xml
+cat << EOF > menu.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<module>
+<menulist>
+<menuitem parent="tools" module="yes" link="" menuid="generator" desc="Generator" order="86" />
+</menulist>
+</module>
+EOF
+
+issabel-menumerge ./menu.xml
+rm -f ./menu.xml
+
+#Copy  all modules to /var/www/html/modules/ de isabell
+cp -r ./Modulos/* /var/www/html/modules/
+
+cd ..
+
+rm -rf A*
